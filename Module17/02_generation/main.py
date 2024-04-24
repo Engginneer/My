99a5fdfc4ca7f6a43e5c48407c62be29d849e534
Владимир <7572vova@mail.ru>
2024-04-24 21:12:20 +0400

    Length

--- Module17/02_generation/main.py
+++ Module17/02_generation/main.py
@@ -2,7 +2,7 @@
 
 if __name__ == '__main__':
 
-    list_lenght = int(input('Введите длину списка: '))
+    list_length = int(input('Введите длину списка: '))
 
-    new_list = [1 if i % 2 == 0 else i % 5 for i in range(list_lenght)]
+    new_list = [1 if i % 2 == 0 else i % 5 for i in range(list_length)]
     print(new_list)
