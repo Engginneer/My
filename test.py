f0d6d5df84fe3ab96ed037ce0a8479bb811a2e8b
Viktor <frostsrost252@mail.ru>
2024-05-29 19:42:10 +0400

    34

--- test.py
+++ test.py
@@ -1,5 +1,14 @@
+def longer(x: str) -> str:
+    word_time = x.split(' ')
+    word_max = word_time[0]
+    for sym in word_time:
+        if len(sym) > len(word_max):
+            word_max = sym
 
-x = "Я"
-text = ''
-text = text + x
-print(text)
+    return word_max
+
+
+if __name__ == '__main__':
+    text = input("Введите текст: ")
+    long_word = longer(text)
+    print(f'Самое длинное слово: "{long_word}"\nЕго длина: {len(long_word)}')
