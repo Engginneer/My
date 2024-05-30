f6646a655f4b42e6a6b6c4fcd1106a578e36442e
Viktor <frostsrost252@mail.ru>
2024-05-30 20:25:21 +0400

    34

---
+++ Module18/04_capital_letters/main.py
@@ -0,0 +1,7 @@
+
+
+if __name__ == '__main__':
+
+    text = input('Введите текст: ')
+    print(text.title())
+
---
+++ Module18/05_password/main.py
@@ -0,0 +1,38 @@
+
+def check_pass():
+
+    password = input('Введите пароль: ')
+
+    if len(password) < 8:
+        print('Пароль ненадежный! Придумайте новый!')
+        check_pass()
+
+    top_sym = True
+    for sym in password:
+        if sym.isupper():
+            top_sym = False
+            break
+    if top_sym:
+        print('Пароль ненадежный! Придумайте новый!')
+        check_pass()
+
+    int_quantity = 0
+
+    for sym in password:
+        if sym.isdigit():
+            int_quantity += 1
+            if int_quantity > 2:
+                break
+
+    if int_quantity < 3:
+        print('Пароль ненадежный! Придумайте новый!')
+        check_pass()
+
+    else:
+        print('Пароль надежный!')
+        quit()
+
+
+
+check_pass()
+
