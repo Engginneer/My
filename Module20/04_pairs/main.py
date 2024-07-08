db5bef291a69ff84fb8fcc2a62dc8c4262fd1106
Viktor <frostsrost252@mail.ru>
2024-07-08 19:27:23 +0400

    completed  module 20 after

--- Module20/04_pairs/main.py
+++ Module20/04_pairs/main.py
@@ -7,14 +7,14 @@ list_rand = [randint(0, 10) for i in range(10)]
 # new_list = []
 #
 # for index, value in enumerate(list_rand):
-#     if index == 0 or index % 2 == 0: # TODO проверка на ноль тут не нужна
+#     if index % 2 == 0:
 #         temp = (value, list_rand[index + 1])
 #         new_list.append(temp)
 #________________________________________________________________________________________
 
 # 2 вариант решения______________________________________________________________________
 
-new_list = [(list_rand[i], list_rand[i + 1]) for i in range(len(list_rand)) if i == 0 or i % 2 == 0]  #TODO здесь можно избавиться от условий указав шаг range(0, len(...), 2)
+new_list = [(list_rand[i], list_rand[i + 1]) for i in range(0, len(list_rand), 2)]
 
 # _______________________________________________________________________________________
 
