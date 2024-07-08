a357203e83fd0c22874b3d4cf0f5a7c7543146fa
Viktor <frostsrost252@mail.ru>
2024-07-08 19:23:32 +0400

    completed  module 20 after

--- Module20/03_players/main.py
+++ Module20/03_players/main.py
@@ -9,5 +9,21 @@ new = []
 for name, numbers in players.items():
     new.append(name + numbers)
 
-print(new)
 
+
+# 1 вариант - ответ не такой, как просят
+new1 = list(zip([i for i in players], [j for j in players.values()]))
+
+# 2 вариант - ответ опять не такой
+new2 = list(zip(players, players.values()))
+
+# 3 вариант - угарно вышло, возможно тут есть что-то дельное)
+new3 = list(zip([(name2 + numbers2) for name2, numbers2 in players.items()]))
+
+
+
+
+print(new)
+print(new1)
+print(new2)
+print(new3)
