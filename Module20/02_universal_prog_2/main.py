bac2953c60088e4bcfb05438c11dd1a29d2a1d8a
Владимир <7572vova@mail.ru>
2024-07-09 18:22:33 +0400

    ЗАмечания по 20ому модулу

--- Module20/02_universal_prog_2/main.py
+++ Module20/02_universal_prog_2/main.py
@@ -1,36 +1,43 @@
-def is_prime(x: int) -> bool:
-    count = 0
-    for i in range(1, x + 1):
-        if x % i == 0:
-            count += 1
-        if count > 2:
-            break
-    if count == 2:
-        return True
-    else:
-        return False
+# def is_prime(x: int) -> bool:
+#     count = 0
+#     for i in range(1, x + 1):
+#         if x % i == 0:
+#             count += 1
+#         if count > 2:
+#             break
+#     if count == 2:
+#         return True
+#     else:
+#         return False
+#
+#
+# def main(object_input):
+#     list_output = []
+#     for index, char in enumerate(object_input):
+#         if is_prime(index):
+#             list_output.append(char)
+#     return list_output
+#
+#
+# object_input = 'О Дивный Новый мир!'
+# list_output = main(object_input)
+# print(list_output)
 
 
-def main(object_input):
-    list_output = []
-    for index, char in enumerate(object_input):
-        if is_prime(index):
-            list_output.append(char)
-    return list_output
+# TODO выполни по этой архитектуре, не просто так оставил, тебе надо написать только одну функцию prime, ввод и вывод функции указан, если очень хочешь, можешь использовать ещё и is_prime
+# TODO и ещё подумай как ускорить твою функцию is_prime, прям базовую утечку ресурса цпу оставил в этой задаче
+# upd. Ну вот внизу нарисовал структуру, названия функций, все необходимое, тебе надо только вместо ... поставить
+# твой код, можешь расширить функцией is_prime дополнительно эту структуру
 
 
-object_input = 'О Дивный Новый мир!'
-list_output = main(object_input)
-print(list_output)
-
-
-#  выполни по этой архитектуре, не просто так оставил, тебе надо написать только одну функцию prime, ввод и вывод функции указан, если очень хочешь, можешь использовать ещё и is_prime
-# и ещё подумай как ускорить твою функцию is_prime, прям базовую утечку ресурса цпу оставил в этой задаче
+def is_prime(num: int) -> bool:
+    ...
 
 
 def prime(maximum: int) -> iter:
     ...
 
 
-inpt = input('Введите фразу для обработки: ')
-print([inpt[i] for i in prime(len(inpt) - 1)])
+if __name__ == '__main__':
+    inpt = input('Введите фразу для обработки: ')
+    print([inpt[i] for i in prime(len(inpt) - 1)])
--- Module20/07_my_zip/main.py
+++ Module20/07_my_zip/main.py
@@ -1,24 +1,24 @@
-def fake_zip(first: iter, second: iter) -> tuple:
-    list_one = tuple(first)
-    list_two = tuple(second)
-
-    new_list = []
-    for i in range(min(len(list_one), len(list_two))):
-        new_list.append((list_one[i], list_two[i]))
-
-    return tuple(new_list)
-
-str_example = {'abcd': 23, 'sdfdsf': 23.5}
-tuple_example = (10, 20, 30, 40)
-
-result_end = fake_zip(str_example, tuple_example)
-
-for itm in result_end:
-    print(itm)
-
-result_easy = zip(str_example, tuple_example)
-for itm in result_easy:
-    print(tuple(itm))
+# def fake_zip(first: iter, second: iter) -> tuple:
+#     list_one = tuple(first)
+#     list_two = tuple(second)
+#
+#     new_list = []
+#     for i in range(min(len(list_one), len(list_two))):
+#         new_list.append((list_one[i], list_two[i]))
+#
+#     return tuple(new_list)
+#
+# str_example = {'abcd': 23, 'sdfdsf': 23.5}
+# tuple_example = (10, 20, 30, 40)
+#
+# result_end = fake_zip(str_example, tuple_example)
+#
+# for itm in result_end:
+#     print(itm)
+#
+# result_easy = zip(str_example, tuple_example)
+# for itm in result_easy:
+#     print(tuple(itm))
 
 #
 # str_example = 'abcd'
@@ -30,20 +30,23 @@ for itm in result_easy:
 
 
 # все ништяк но верни форму которую я тебе передал:
+# TODO ну ты чет опять, просто закомментил то что я написал и
+#  все, ну я зря что ли это делаю? я в целом могу не делать, просто буду чекать на факт, работает или нет и все,
+#  но это ты и сам проверить можешь
 
-# def zipper(first: iter, second: iter) -> tuple:
-#     ...
-#
-#
-# first_data = 'asd'
-# secondary_data = (10, 20, 30, 40)
-#
-# res = zip(first_data, secondary_data)
-# for itm in res:
-#     print(tuple(itm))
-#
-# print('\n\n')
-#
-# res1 = zipper(first_data, secondary_data)
-# for itm in res1:
-#     print(tuple(itm))
+def zipper(first: iter, second: iter) -> tuple:
+    ...
+
+
+first_data = 'asd'
+secondary_data = (10, 20, 30, 40)
+
+res = zip(first_data, secondary_data)
+for itm in res:
+    print(tuple(itm))
+
+print('\n\n')
+
+res1 = zipper(first_data, secondary_data)
+for itm in res1:
+    print(tuple(itm))
