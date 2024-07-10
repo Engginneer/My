ffce05f83492f7783775191ad13ea560b267e3fe
Владимир <7572vova@mail.ru>
2024-07-10 14:48:06 +0400

    21ый модуль

---
+++ Module21/01_challenge_2/main.py
@@ -0,0 +1,7 @@
+
+def printer(num: int) -> str:
+    ...
+
+
+src = int(input('Введите число: '))
+print(printer(src))
---
+++ Module21/04_advanced_sum/main.py
@@ -0,0 +1,7 @@
+
+def custom_sum(obj: list):
+    ...
+
+
+num_lst = [[1, 2, [3]], [1, 5, (15, 0), 'hi'], 3]
+print(custom_sum(num_lst))
