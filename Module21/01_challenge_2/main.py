ca79881a25f524630603c6d590b93d19b65b24c9
Viktor <frostsrost252@mail.ru>
2024-07-20 15:30:30 +0400

    working in module 21 (completed first task)

--- Module21/01_challenge_2/main.py
+++ Module21/01_challenge_2/main.py
@@ -1,6 +1,9 @@
-
 def printer(num: int) -> str:
-    ...
+    if num == 1:
+        str_temp = str(src - num + 1) + '\n'
+        return str_temp
+    else:
+        return str(src - num + 1) + '\n' + printer(num - 1)
 
 
 src = int(input('Введите число: '))
