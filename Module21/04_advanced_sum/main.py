978be59e2958acbaec3292305e2150050e32aac6
Viktor <frostsrost252@mail.ru>
2024-07-22 20:45:33 +0400

    working in module 21 (completed fourth task)

--- Module21/04_advanced_sum/main.py
+++ Module21/04_advanced_sum/main.py
@@ -1,6 +1,12 @@
 
 def custom_sum(obj: list):
-    ...
+    sum_out = 0
+    for index in obj:
+        if isinstance(index, (int, float)):
+            sum_out += index
+        elif isinstance(index, (list, tuple, set)):
+            sum_out += custom_sum(index)
+    return sum_out
 
 
 num_lst = [[1, 2, [3]], [1, 5, (15, 0), 'hi'], 3]
