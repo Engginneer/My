a226108b5ad44d748cce3605aa522fb700e16d1e
Viktor <frostsrost252@mail.ru>
2024-07-23 14:01:38 +0400

    working in module 21 (completed the fifth task)

--- Module21/05_list_of_lists_2/main.py
+++ Module21/05_list_of_lists_2/main.py
@@ -3,7 +3,16 @@ nice_list = [1, 2, [3, 4], [[5, 6, 7], [8, 9, 10]],
 
 
 def defolder(obj: list) -> list:
-    ...
+    new_list = []
+    for i in obj:
+        if isinstance(i, (int, float)):
+            new_list.append(i)
+        elif isinstance(i, (list, tuple, set)):
+            for elem in defolder(i):
+                new_list.append(elem)
+    return new_list
+
+
 
 
 if __name__ == '__main__':
