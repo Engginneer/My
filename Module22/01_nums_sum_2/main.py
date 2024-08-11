e2a3873adff633608598d7711a4735397cac9ec5
Viktor <frostsrost252@mail.ru>
2024-08-11 12:17:33 +0400

    completed first task from 22 module

--- Module22/01_nums_sum_2/main.py
+++ Module22/01_nums_sum_2/main.py
@@ -1,2 +1,10 @@
 
+file = open('numbers.txt', 'r')
+summ = 0
+str_file = file.read()
+for i in str_file:
+    if i.isdigit():
+        summ += int(i)
 
+file_answer = open('answer.txt', 'w')
+file_answer.write(str(summ))
