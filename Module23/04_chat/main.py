15ab05d13fc5ae4d6069dd115fb85b6b967ec041
Владимир <7572vova@mail.ru>
2024-08-25 15:01:40 +0400

    Markdowns 4 23rd module

--- Module23/04_chat/main.py
+++ Module23/04_chat/main.py
@@ -5,7 +5,8 @@ with open('chat.txt', 'a', encoding='utf8') as chat:
         menu_item = int(input('\n1. Просмотреть текущий чат\n2. Отправить сообщение\n'
                               '3. Завершить сессию\n'))
         if menu_item == 1:
-            chat.close()
+            chat.close() # TODO Как же отвратительно это выглядит, а главное после первого чтения
+                         #  программа перестанет работать
             with open('chat.txt', 'r', encoding='utf8') as chat_temp:
                 for i_elem in chat_temp:
                     temp_list = i_elem.split(' ')
