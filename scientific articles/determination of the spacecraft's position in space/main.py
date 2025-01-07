a98d9bcad3d9c077de81753ccc99344a9d0ef144
Viktor <frostsrost252@mail.ru>
2025-01-07 19:43:13 +0400

    working on an article

--- scientific articles/determination of the spacecraft's position in space/main.py
+++ scientific articles/determination of the spacecraft's position in space/main.py
@@ -2,36 +2,49 @@ from random import randint, uniform
 from pandas import read_excel
 
 
-def find_cos(curr, volt) -> float:
-    power_fact = 0.09  # Фактическая мощность имеющейся БФ
-    # (константа, известна на стадии изготовления)
+def unpacking_tmi_iu(name_file: str):  # функция распаковывает телеметрию из txt и находит мощность
+    current_list = []
+    voltage_list = []
+    cosa_list = []
+    with open(name_file, 'r', encoding='utf8') as data_TMI:  # Распаковываем телеметрию СЭП
+        # считаем значения мощности по каждой точке, после чего находим значение cosa
+        for i in data_TMI:
+            current_list.append(round(float(i.split(' ')[0]) / 1000, 4))
+            voltage_list.append(round(float(i.split(' ')[1]), 4))
+        for i in range(len(current_list)):
+            cosa_list.append(round(find_cosa(current_list[i], voltage_list[i]), 4))
+
+
+def find_cosa(curr, volt) -> float:  # Функция находит значение cosa
+    # (где а - угол между нормалью к БС и направлением на Солнце)
+    power_fact = 0.09  # Фактическая мощность имеющейся БФ(константа, известна на стадии изготовления)
     power_moment = curr * volt
     cosa = power_moment / power_fact
     return cosa
+
+
+def find_degree(power):  # Функция находит максимально схожее теоретическое значение с получившимся
+    # и выдаёт предполагаемые значения углов по осям X и Y
+    base_cos = read_excel('base_cos.xlsx')
+    min_diff = 1
+    result_x = 0
+    result_y = 0
+    for x in range(0, 91, 5):
+        for y in range(1, 19):
+            if abs(power - base_cos[x][y]) < min_diff:
+                min_diff = abs(power - base_cos[x][y])
+                result_y = x
+                result_x = y * 5
+    return result_x, result_y
 
 
 if __name__ == '__main__':
     main_system = False  # Имитация отказа основной системы
 
     if main_system == False:
-        Current_list = []
-        Voltage_list = []
-        cosa_list = []
-        with open('data.txt', 'r', encoding='utf8') as data_TMI:  # Распаковываем телеметрию СЭП
-            for i in data_TMI:
-                Current_list.append(round(float(i.split(' ')[0])/1000, 4))
-                Voltage_list.append(round(float(i.split(' ')[1]), 4))
-            print(Current_list)
-            print(Voltage_list)
-            for i in range(len(Current_list)):
-                cosa_list.append(round(find_cos(Current_list[i], Voltage_list[i]), 4))
-            print(cosa_list)
+        print(1)
 
-        base_cos = read_excel('base_cos.xlsx')
-        angle = 0.964
-        for i in range(0, 91, 5):
-            for i in base_cos[5]:
-                # здесь нужно сравнивать косинус полученный с таблией и выбирать самый ближайший, чтобы определить угол.
-
-
-
+# Нужно написать функцию для распаковки ТМИ, где уже есть мощность
+# Написать функцию, которая случайно рисует синтезированные данные по мощности
+# запустить код на синтезированных данных
+# изобразить график, где будет две кривые, одна X, вторая Y, в зависимости от мощности или времени, надо подумать
--- scientific articles/determination of the spacecraft's position in space/test.py
+++ scientific articles/determination of the spacecraft's position in space/test.py
@@ -1,6 +1,16 @@
-with open('data.txt', 'r', encoding='utf8') as chat_temp:
-    new_list = list(chat_temp)
-    x = new_list[0]
-    print(x)
-    x = x.split(' ')
-    print(x[0])
+import pandas
+
+base_cos = pandas.read_excel('base_cos.xlsx')
+angle = 0.587
+min_diff = 1
+result_X = 0
+result_Y = 0
+for x in range(0, 91, 5):
+    for y in range(1, 19):
+        if abs(angle - base_cos[x][y]) < min_diff:
+            min_diff = abs(angle - base_cos[x][y])
+            result_X = x
+            result_Y = y * 5
+
+print(result_X, result_Y)
+
