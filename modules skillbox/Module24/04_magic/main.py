d409708303b956a44a3ee2724baed952430e642e
Viktor <frostsrost252@mail.ru>
2025-01-12 22:14:23 +0400

    working on tasks skillbox (24.4)

--- modules skillbox/Module24/04_magic/main.py
+++ modules skillbox/Module24/04_magic/main.py
@@ -1,1 +1,21 @@
 # TODO здесь писать код
+class Fire:
+    def __add__(self, Water):
+        return Steam
+
+
+class Water:
+    def __add__(self, Fire):
+        return Steam
+
+# class Earth:
+#
+#
+# class Air:
+#
+#
+class Steam:
+    print(f'Сложили воду и огонь, получили пар')
+
+
+print(Fire() + Water())
