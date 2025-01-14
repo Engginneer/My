1f9fe046db8a243b1367593344d8a77a6077f41f
Viktor <frostsrost252@mail.ru>
2025-01-14 14:09:37 +0400

    working on tasks skillbox (24.4)

--- fast_test.py
+++ fast_test.py
@@ -1,78 +1,11 @@
-from random import randint
-from time import sleep
+class test:
+    def __init__(self, value):
+        self.value = value
+
+    def __add__(self, other):
+        return self.value + other
 
 
-class Parent:
+x = test(3) + 3 + 5
 
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-        self.childs = []
-
-    def relax_child(self, child):
-        child.relax()
-
-    def feed_child(self, child):
-        child.feed()
-
-    def check_babies(self):
-        for baby in self.childs:
-            if baby.crying():
-                if baby.hungry:
-                    print(f'{baby.name} голоден')
-                    baby.feed()
-                    print(f'{self.name} покормил {baby.name}')
-
-                elif not baby.relax_state:
-                    print(f'{baby.name} не спокоен')
-                    baby.feed()
-                    print(f'{self.name} успокоил {baby.name}')
-
-            else:
-                print(f'{baby.name} спит')
-
-class Child:
-
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-        self.relax_state = True
-        self.hungry = False
-
-    def relax(self):
-        self.relax_state = True
-
-    def feed(self):
-        self.hungry = False
-
-    def rand_states(self):
-        if 0 < randint(0, 100) < 50:
-            self.hungry = True
-
-        if 0 < randint(0, 100) < 50:
-            self.relax_state = False
-
-    def crying(self):
-        if self.relax_state == False or self.hungry == True:
-            print(f'Ребенок {self.name} плачет')
-            return True
-
-        return False
-
-
-if __name__ == '__main__':
-
-    viktor = Parent('Vikotr', 29)
-    natalya = Parent('Natalya', 28)
-    stepa = Child('Stepan', 0)
-    viktor.childs.append(stepa)
-    natalya.childs.append(stepa)
-    while True:
-        stepa.rand_states()
-        if 0 < randint(0, 100) < 50:
-            viktor.check_babies()
-
-        else:
-            natalya.check_babies()
-        sleep(2)
-        print('\n')
+print(x)
--- modules skillbox/Module24/04_magic/main.py
+++ modules skillbox/Module24/04_magic/main.py
@@ -1,21 +1,114 @@
 # TODO здесь писать код
+# Вот таблица преобразований:
+#
+# Вода + Воздух = Шторм
+# Вода + Огонь = Пар
+# Вода + Земля = Грязь
+# Воздух + Огонь = Молния
+# Воздух + Земля = Пыль
+# Огонь + Земля = Лава
 class Fire:
-    def __add__(self, Water):
-        return Steam
+    def __add__(self, other):
+        if isinstance(other, Water):
+            return Steam
+        elif isinstance(other, Earth):
+            return Lava
+        elif isinstance(other, Air):
+            return Lightning
+        else:
+            return None
 
 
 class Water:
-    def __add__(self, Fire):
-        return Steam
+    def __add__(self, other):
+        if isinstance(other, Fire):
+            return Steam
+        elif isinstance(other, Earth):
+            return Dirt
+        elif isinstance(other, Fire):
+            return Storm
+        elif isinstance(other, Frost):
+            return Ice
 
-# class Earth:
-#
-#
-# class Air:
+
+class Earth:
+    def __add__(self, other):
+        if isinstance(other, Water):
+            return Dirt
+        elif isinstance(other, Air):
+            return Dust
+        elif isinstance(other, Fire):
+            return Lava
+
+
+class Air:
+    def __add__(self, other):
+        if isinstance(other, Water):
+            return Storm
+        elif isinstance(other, Earth):
+            return Dust
+        elif isinstance(other, Fire):
+            return Lightning
+        elif isinstance(other, Frost):
+            return Blizzard
+
+
+class Frost:
+    def __add__(self, other):
+        if isinstance(other, Water):
+            return Ice
+        elif isinstance(other, Air):
+            return Blizzard
+
+
 #
 #
 class Steam:
-    print(f'Сложили воду и огонь, получили пар')
+    def __str__(self):
+        return 'Пар'
 
 
-print(Fire() + Water())
+class Lava:
+    def __str__(self):
+        return 'Лава'
+
+
+class Storm:
+    def __str__(self):
+        return 'Шторм'
+
+
+class Dust:
+    def __str__(self):
+        return 'Пыль'
+
+
+class Dirt:
+    def __str__(self):
+        return 'грязь'
+
+
+class Lightning:
+    def __str__(self):
+        return 'Молния'
+
+
+class Ice:
+    def __str__(self):
+        return 'Лёд'
+
+
+class Blizzard:
+    def __str__(self):
+        return 'Вьюга'
+
+
+fire = Fire()
+water = Water()
+earth = Earth()
+air = Air()
+frost = Frost()
+x = water + frost
+print(x)  #Почему он мне выдаёт класс - как объект, а не как строковое представление через __str__?
+print(x()) # А вот так выдает правильно, почему?? Причем если несуществующую комбинацию поставить,
+# в этом принте он выдаст ошибку
