2445f9b8e1263e1cfe36b81b04b9db6ebcc5a600
Viktor <frostsrost252@mail.ru>
2025-01-14 15:18:22 +0400

    working on tasks skillbox (24.5)

--- fast_test.py
+++ fast_test.py
@@ -1,11 +1,93 @@
-class test:
-    def __init__(self, value):
-        self.value = value
-
-    def __add__(self, other):
-        return self.value + other
+from random import randint
 
 
-x = test(3) + 3 + 5
+class Human:
 
-print(x)
+    def __init__(self, name):
+        self.name = name
+        self.__fullness = 50
+        self.__home = None
+
+    def set_home(self, house):
+        self.__home = house
+
+    def eat(self):
+        self.__home.set_fridge(self.__home.get_fridge() - 5)
+        self.__fullness += 10
+
+    def work(self):
+        self.__fullness -= 5
+        self.__home.set_money(self.__home.get_money() + 15)
+
+    def play(self):
+        self.__fullness -= 5
+
+    def shopping(self):
+        self.__home.set_money(self.__home.get_money() - 5)
+        self.__home.set_fridge(self.__home.get_fridge() + 15)
+
+    def action_roll(self):
+
+        action = randint(1,6)
+        print(f'{self.name} текущая сытость - {self.__fullness}, '
+              f'остаток денег - {self.__home.get_money()}, '
+              f'остаток еды - {self.__home.get_fridge()} ', end='')
+
+        if self.__fullness <= 0:
+            print(f'{self.name} умер(ла) от голода')
+
+        elif self.__fullness < 10:
+            print(f'низкая сытость - кушаем')
+            self.eat()
+
+        elif self.__home.get_fridge() < 10:
+            print('в холодильнике мало еды - закупаемся')
+            self.shopping()
+
+        elif self.__home.get_money() < 50:
+            print('дома мало денег - работаем')
+            self.work()
+
+        elif action == 1:
+            self.work()
+            print('все в порядке, решил(а) сходить на работу')
+
+        elif action == 2:
+            self.eat()
+            print('все в порядке, решил(а) перекусить')
+
+        else:
+            self.play()
+            print('все в порядке, решил(а) поиграться')
+
+
+class Home:
+
+    def __init__(self):
+        self.__fridge = 50
+        self.__money = 0
+
+    def get_fridge(self):
+        return self.__fridge
+
+    def set_fridge(self, new_value):
+        self.__fridge = new_value
+
+    def get_money(self):
+        return self.__money
+
+    def set_money(self, new_value):
+        self.__money = new_value
+
+
+if __name__ == '__main__':
+
+    oleg = Human('Oleg')
+    anna = Human('Anna')
+    home = Home()
+    oleg.set_home(home)
+    anna.set_home(home)
+    for i in range(365):
+        print(f'\nДень {i}')
+        oleg.action_roll()
+        anna.action_roll()
--- modules skillbox/Module24/05_cohabitation/main.py
+++ modules skillbox/Module24/05_cohabitation/main.py
@@ -1,1 +1,72 @@
 # TODO здесь писать код
+from random import randint
+
+
+class Human:
+    def __init__(self, name, house, satiety=50):
+        self.name = name
+        self.satiety = satiety
+        self.house = house
+
+    def to_eat(self):
+        self.satiety += 1
+        self.house.eat_in_fridge -= 1
+
+    def go_to_work(self):
+        self.satiety -= 1
+        self.house.money += 1
+
+    def go_to_market(self):
+        self.house.money -= 1
+        self.house.eat_in_fridge += 1
+
+    def to_game(self):
+        self.satiety -= 1
+
+    def day(self):
+        print(f'Сытость {hum.name} = {hum.satiety}')
+        x = randint(1, 6)
+        if hum.satiety < 20:
+            hum.to_eat()
+            print(f'{hum.name} поел, сытость = {hum.satiety}')
+        elif house.eat_in_fridge < 10:
+            hum.go_to_market()
+            print(f'{hum.name} сходил в магазин, еды стало - {hum.house.eat_in_fridge}')
+        elif house.money < 50:
+            hum.go_to_work()
+            print(f'{hum.name} подзаработал, теперь денег -  {hum.house.money}')
+        elif x == 1:
+            hum.go_to_work()
+            print(f'{hum.name} подзаработал, теперь денег -  {hum.house.money}')
+        elif x == 2:
+            hum.to_eat()
+            print(f'{hum.name} поел, сытость = {hum.satiety}')
+        else:
+            hum.to_game()
+            print(f'{hum.name} поиграл, сытость = {hum.satiety}')
+
+
+class House:
+    def __init__(self, eat_in_fridge=50, money=0):
+        self.eat_in_fridge = eat_in_fridge
+        self.money = money
+        self.list_lodger = []
+
+
+if __name__ == '__main__':
+    house = House()
+    Feel = Human('Фил', house)
+    Amanda = Human('Аманда', house)
+    house.list_lodger.append(Feel)
+    house.list_lodger.append(Amanda)
+    for i in range(365):
+        print(f'День № {i + 1}')
+        print()
+        print(f'Денег осталось - {house.money}')
+        print(f'Еды в холодильнике осталось - {house.eat_in_fridge}')
+        for hum in house.list_lodger:
+            if hum.satiety <= 0:
+                print(f'{hum} - умер(ла) от голода.')
+                break
+            hum.day()
+        print()
