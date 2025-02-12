6b31bd929ba5d00e0aa42b04392b290c192c8974
Viktor <frostsrost252@mail.ru>
2025-02-12 17:57:36 +0400

    test

--- fast_test.py
+++ fast_test.py
@@ -1,2 +1,10 @@
-x = [[0 for i in range(3)] for j in range(3)]
-print(x)
+sequence = int(input('Введите количество чисел? '))
+numCount = 0
+for num in range(sequence):
+    print('Введите', num + 1, '-е число: ', end='')
+    number = int(input())
+    for n in range(2, number):
+        if number % n == 0:
+            numCount += 1
+            break
+print('Количество простых чисел в последовательности: ', numCount)
